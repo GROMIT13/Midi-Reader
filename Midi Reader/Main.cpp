47dbeea51127d62313c0049b2482fc96ba5c6697
GROMIT13 <przem9.pp@gmail.com>
2024-08-13 22:14:15 +0200

    Added GConsole

--- Midi Reader/Main.cpp
+++ Midi Reader/Main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <format>
 #include <Windows.h>
-#include <mmeapi.h>
+#include "Engine/GConsole.hpp"
+#include "Engine/Midi/Midi.hpp"
 
 enum MidiEvent
 {
@@ -30,182 +31,39 @@ struct MidiData
 	unsigned long timestamp;
 };
 
-void PrintMidiData(unsigned char midiByte1, unsigned char midiByte2, unsigned char midiByte3)
+void PrintData(unsigned char midiByte1, unsigned char midiByte2, unsigned char midiByte3)
 {
 	std::cout << std::format("MIDI MSG {:x} {:x} {:x}", midiByte1, midiByte2, midiByte3) << std::endl;
 }
 
-void PrintMidiData(const MidiData &midiData)
+void PrintData(const Midi::Data &midiData)
 {
 	std::cout << std::format("MIDI MSG {:x} {:x} {:x}", midiData.byte1, midiData.byte2, midiData.byte3) << std::endl;
 }
 
-void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, MidiData* midiDataBuffer, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
-{
-
-	switch (wMsg)
-	{
-		case MIM_CLOSE :
-		{
-			std::cout << "MIDI device closed" << std::endl;
-			break;
-		}
-		case MIM_DATA :
-		{
-			static int i = 0;
-			MidiData midiData = { 0 };
-
-			midiDataBuffer[128].byte2 = 1;
-			midiData.byte1 = (dwParam1 & 0xFF);
-			midiData.byte2 = ((dwParam1 >> 8) & 0xFF);
-			midiData.byte3 = ((dwParam1 >> 16) & 0xFF);
-			midiData.timestamp = (unsigned long)dwParam2;
-			memcpy(&midiDataBuffer[i], &midiData, sizeof(MidiData));
-			i++;
-			i %= 128;
-			midiDataBuffer[128].byte1 = i;
-			//PrintMidiData(midiByte1, midiByte2, midiByte3);
-			break;
-		}
-		case MIM_ERROR :
-		{
-			std::cout << "MIM_ERROR" << std::endl;
-			break;
-		}
-		case MIM_LONGDATA :
-		{
-			std::cout << "MIM_LONGDATA" << std::endl;
-			break;
-		}
-		case MIM_LONGERROR :
-		{
-			std::cout << "MIM_LONGERROR" << std::endl;
-			break;
-		}
-		case MIM_MOREDATA :
-		{
-			std::cout << "MIM MOREDATA" << std::endl;
-			break;
-		}
-		case MIM_OPEN :
-		{
-			std::cout << "MIDI device opened" << std::endl;
-			break;
-		}
-
-		default:
-			return;
-	}
-}
-
-// Scans for MIDI devices, returns device ID choosen by user
-// if no devices are found retuns -1 
-int ChooseDevice()
-{
-	int numOfDevices = (int)midiInGetNumDevs();
-
-	MIDIINCAPS* pmic = new MIDIINCAPS;
-
-	MMRESULT mmresult;
-
-	int choice = 0;
-
-	for (int i = 0; i < numOfDevices; i++)
-	{
-		mmresult = midiInGetDevCaps(i, pmic, sizeof(MIDIINCAPS));
-
-		if (mmresult == MMSYSERR_BADDEVICEID)
-			std::cout << "The specified device identifier is out of range." << std::endl;
-		if (mmresult == MMSYSERR_INVALPARAM)
-			std::cout << "The specified pointer or structure is invalid." << std::endl;
-		if (mmresult == MMSYSERR_NODRIVER)
-			std::cout << "The driver is not installed." << std::endl;
-		if (mmresult == MMSYSERR_NOMEM)
-			std::cout << "The system is unable to allocate or lock memory." << std::endl;
-
-		std::cout << "[" << i << "] ";
-
-		wchar_t name[32];
-
-		for (int i = 0; i < 32; i++)
-			name[i] = ((wchar_t)(pmic->szPname[i]));
-		std::wcout << name << std::endl;
-	}
-
-	delete pmic;
-
-	if (numOfDevices <= 0)
-	{
-		std::cout << "No MIDI devices detected" << std::endl;
-		return -1;
-	}
-
-	std::cout << "choose MIDI device:";
-	std::cin >> choice;
-
-	if (choice < 0 || choice >= numOfDevices)
-	{
-		std::cout << "Device with this ID does not exist, choosing ID 0" << std::endl;
-		choice = 0;
-	}
-
-	return choice;
-}
-
 int main()
 {
+	GConsole screen("MIDI Reader",120,20,10,20);
+	Midi midi;
 
-	HMIDIIN hmidiin;
-	MMRESULT midiInOpenLog;
-	MidiData* midiDataBuffer = new MidiData[129];
-	midiDataBuffer[128].byte1 = 0; //byte1 keeps track of last updated variable in buffer
-	midiDataBuffer[128].byte2 = 0; //byte2 checks if first variable in buffer was initialized (0 - false, other - true)
-	unsigned char midiDataIndex = 0;
-	unsigned char midiDataIndexDestination = 0;
+	screen.ShowConsoleCursor(true);
 
-	int deviceID = ChooseDevice();
-	if (deviceID == -1)
-		return -1;
-	midiInOpenLog = midiInOpen(&hmidiin, deviceID, (DWORD_PTR)MidiInProc, (DWORD_PTR)midiDataBuffer, CALLBACK_FUNCTION);
-
-	if(midiInOpenLog != MMSYSERR_NOERROR)
-	{
-		std::cout << "Failed to open MIDI port" << std::endl;
-		return -1;
+	if (midi.InOpen() == -1) {
+		std::cin.get();
+		return 0;
 	}
 
-	midiInStart(hmidiin);
+	screen.ShowConsoleCursor(false);
 
-	//Wait for fist midi command
-	while (midiDataBuffer[128].byte2 == 0)
-		continue;
-
-	while (true)
+	while (!GetAsyncKeyState(VK_ESCAPE))
 	{
-		if (GetAsyncKeyState('q'))
-			break;
+		Midi::Data* midiData = midi.GetMidiInData();
+
+		if (midiData == nullptr)
+			continue;
 		
-		if (midiDataIndex != midiDataIndexDestination)
-		{
-			//read event here
-			PrintMidiData(midiDataBuffer[midiDataIndex]);
-			midiDataIndex++;
-			midiDataIndex %= 128;
-		}
 
-		/*for (int i = 0; i < 16; i++)
-		{
-			PrintMidiData(midiDataBuffer[i]);
-		}
-		system("cls");*/
-
-		//std::cout << (int)midiDataIndex << " " << (int)midiDataIndexDestination << std::endl;
-		midiDataIndexDestination = midiDataBuffer[128].byte1;
-		//Sleep(50);
+		screen.UpdateScreen();
 	}
 
-	midiInStop(hmidiin);
-	midiInClose(hmidiin);
-
-	
 }
