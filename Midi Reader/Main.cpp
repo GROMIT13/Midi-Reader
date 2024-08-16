2515338bc87c812c3b27eb6be22bdefd597cbbaa
GROMIT13 <przem9.pp@gmail.com>
2024-08-17 00:09:43 +0200

    pressed notes are now showing on screen

--- Midi Reader/Main.cpp
+++ Midi Reader/Main.cpp
@@ -1,15 +1,9 @@
 ﻿#include <iostream>
 #include <format>
+#include <vector>
 #include <Windows.h>
 #include "Engine/GConsole.hpp"
 #include "Engine/Midi/Midi.hpp"
-
-enum class Accidental
-{
-	Flat = 0x266D,
-	Natural = 0x266E,
-	Sharp = 0x266F
-};
 
 int main()
 {
@@ -17,6 +11,9 @@ int main()
 	Midi midi;
 	PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_AQUA, FG_COLOR_BLACK, FG_COLOR_BLUE };
 	Midi::Note* notes = new Midi::Note[128];
+	std::vector<unsigned char> activeNoteList; //value represents note number that is currently on
+	activeNoteList.reserve(128);
+	//std::cout << activeNoteList.size();
 
 
 	screen.ShowConsoleCursor(true);
@@ -38,15 +35,47 @@ int main()
 
 		if (midiData == nullptr)
 			continue;
-		midi.PrintData(*midiData);
 
 		if ((midiData->byte1 & 0xF0) == Midi::Event::NoteOff)
 		{
+			if(midi.GetIsSustainPedalPressed())
+				notes[midiData->byte2].state = Midi::State::Sustain;
+			else
+			{
+				notes[midiData->byte2].state = Midi::State::Off;
+				for (int i = 0; i < activeNoteList.size(); i++)
+					if (activeNoteList[i] == midiData->byte2)
+					{
+						activeNoteList.erase(activeNoteList.begin() + i);
+						screen.DrawString(3, 1 + activeNoteList.size(), "   ");
+						break;
+					}
 
+			}	
+
+			notes[midiData->byte2].velocity = midiData->byte3;
 		}
 		else if ((midiData->byte1 & 0xF0) == Midi::Event::NoteOn)
 		{
-		
+			notes[midiData->byte2].state = Midi::State::On;
+			notes[midiData->byte2].velocity = midiData->byte3;
+
+			if (activeNoteList.size() == 0)
+				activeNoteList.push_back(midiData->byte2);
+			else
+			{
+				int i;
+				for (i = 0; i < activeNoteList.size(); i++)
+				{
+					if (activeNoteList[i] < midiData->byte2)
+					{
+						activeNoteList.insert(activeNoteList.begin() + i, midiData->byte2);
+						break;
+					}
+				}
+				if (i == activeNoteList.size())
+					activeNoteList.push_back(midiData->byte2);
+			}
 		}
 		else if ((midiData->byte1 & 0xF0) == Midi::Event::PolyphonicAftertouch)
 		{
@@ -61,7 +90,15 @@ int main()
 			}
 			else
 			{
-
+				for (int i = 0; i < activeNoteList.size(); i++)
+				{
+					if (notes[activeNoteList[i]].state == Midi::State::Sustain)
+					{
+						activeNoteList.erase(activeNoteList.begin() + i);
+						screen.DrawString(3, 1 + activeNoteList.size(), "   ");
+						i--;
+					}
+				}
 			}
 		}
 		else if ((midiData->byte1 & 0xF0) == Midi::Event::ProgramChange)
@@ -80,6 +117,9 @@ int main()
 		{
 
 		}
+
+		for (int i = 0; i < activeNoteList.size(); i++)
+			screen.DrawNoteName(3, 1 + i, activeNoteList[i], FG_COLOR_WHITE);
 
 		screen.UpdateScreen();
 	}
