c8ca0753e3f9dc314af9eb1559e92e765b7b83bf
GROMIT13 <przem9.pp@gmail.com>
2024-08-17 02:02:32 +0200

    Piano now updates on midi events

--- Midi Reader/Main.cpp
+++ Midi Reader/Main.cpp
@@ -9,12 +9,17 @@ int main()
 {
 	GConsole screen("MIDI Reader", 120, 20, 15, 30, L"DejaVU Sans Mono");
 	Midi midi;
-	PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_AQUA, FG_COLOR_BLACK, FG_COLOR_BLUE };
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_AQUA, FG_COLOR_BLACK, FG_COLOR_LIGHT_BLUE };			// BLUE + AQUA
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_BLUE, FG_COLOR_BLACK, FG_COLOR_BLUE };			// BLUE 
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_AQUA, FG_COLOR_BLACK, FG_COLOR_LIGHT_AQUA };			// AQUA
+	PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_RED, FG_COLOR_BLACK, FG_COLOR_RED };				// RED
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_PURPLE, FG_COLOR_BLACK, FG_COLOR_PURPLE };		// PURPLE
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_GREEN, FG_COLOR_BLACK, FG_COLOR_GREEN };			// GREEN
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_YELLOW, FG_COLOR_BLACK, FG_COLOR_YELLOW };		// YELLOW
+	//PianoKeyColor pianoColor = { FG_COLOR_WHITE, FG_COLOR_LIGHT_GRAY, FG_COLOR_BLACK, FG_COLOR_GRAY };			//GRAY
 	Midi::Note* notes = new Midi::Note[128];
 	std::vector<unsigned char> activeNoteList; //value represents note number that is currently on
 	activeNoteList.reserve(128);
-	//std::cout << activeNoteList.size();
-
 
 	screen.ShowConsoleCursor(true);
 
@@ -25,7 +30,9 @@ int main()
 	}
 
 	screen.ShowConsoleCursor(false);
-	screen.Fill(CHARACTER_FULL, FG_COLOR_BLACK);
+	screen.Fill(CHARACTER_FULL, FG_COLOR_BLACK); // value 0 in DejaVU Sans Mono looks weird for some reason
+	for (int i = 0; i < 16; i++)
+		screen.Draw(i, 0, CHARACTER_FULL, i);
 	screen.DrawPiano(7, screen.GetDimension().y - 5, 7, pianoColor);
 	screen.UpdateScreen();
 
@@ -46,6 +53,7 @@ int main()
 				for (int i = 0; i < activeNoteList.size(); i++)
 					if (activeNoteList[i] == midiData->byte2)
 					{
+						screen.DrawPianoKeyNote(-2, screen.GetDimension().y - 4, CHARACTER_FULL, pianoColor.whiteRelased, pianoColor.blackRelased, activeNoteList[i]);
 						activeNoteList.erase(activeNoteList.begin() + i);
 						screen.DrawString(3, 1 + activeNoteList.size(), "   ");
 						break;
@@ -60,6 +68,10 @@ int main()
 			notes[midiData->byte2].state = Midi::State::On;
 			notes[midiData->byte2].velocity = midiData->byte3;
 
+			//Draw Key
+			screen.DrawPianoKeyNote(-2, screen.GetDimension().y - 4, CHARACTER_FULL, pianoColor.whitePressed, pianoColor.blackPressed, midiData->byte2);
+
+			//Set activeNoteList
 			if (activeNoteList.size() == 0)
 				activeNoteList.push_back(midiData->byte2);
 			else
@@ -94,6 +106,7 @@ int main()
 				{
 					if (notes[activeNoteList[i]].state == Midi::State::Sustain)
 					{
+						screen.DrawPianoKeyNote(-2, screen.GetDimension().y - 4, CHARACTER_FULL, pianoColor.whiteRelased, pianoColor.blackRelased, activeNoteList[i]);
 						activeNoteList.erase(activeNoteList.begin() + i);
 						screen.DrawString(3, 1 + activeNoteList.size(), "   ");
 						i--;
